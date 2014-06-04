ae0b0f2e8701f5c0cf663e324d9e4b2377ad67f6
sivaprakashr <sivaprakashr@aditi.com>
2014-06-04 09:53:19 +0530

    Commit Solution

---
+++ TestFlipKart/BrowserLauncher.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFlipKart
+{
+    class BrowserLauncher
+    {
+        [ClassInitialize]
+        public static void LaunchBroswer(TestContext context)
+        {
+            Playback.Initialize();
+            BrowserWindow browser = BrowserWindow.Launch(new System.Uri("about:blank"));
+        }
+    }
+}
